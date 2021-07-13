199afabc8372ee5a1ddfc304492032d3d14f7f43
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-07-13 22:05:52 +0200

    Add post pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,11 @@ import { Layout } from "../components/Layout";
 import NextLink from "next/link";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 10,
+    },
+  });
   return (
     <Layout>
       <Link>
