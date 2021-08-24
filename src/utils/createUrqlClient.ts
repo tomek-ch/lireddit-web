feca46426d3604c538eefb9d2e5392f0a45d55d2
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-08-24 18:41:19 +0200

    Handle pagination cache invalidation on post add

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -80,11 +80,13 @@ export const createUrqlClient = (ssrExchange: any) => ({
       updates: {
         Mutation: {
           createPost: (_result, args, cache, info) => {
-            console.log(cache.inspectFields("Query"));
-            cache.invalidate("Query", "posts", {
-              limit: 15,
+            const allFields = cache.inspectFields("Query");
+            const fieldInfos = allFields.filter(
+              (info) => info.fieldName === "posts"
+            );
+            fieldInfos.forEach((fi) => {
+              cache.invalidate("Query", "posts", fi.arguments);
             });
-            console.log(cache.inspectFields("Query"));
           },
           logout: (_result, args, cache, info) => {
             betterUpdateQuery<LogoutMutation, MeQuery>(
